380a521c25335dc32d9141fbd5f93f2b7c95005e
csabimvp <kellercsabii@gmail.com>
2026-09-02 15:21:12 +0100

    Add macOS app shell

---
+++ PassFort/PassFortApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassFortApp.swift
+//  PassFort
+//
+//  Created by Csaba Keller on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PassFortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
